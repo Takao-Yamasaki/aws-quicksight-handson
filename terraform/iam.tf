6425be943e8acc441d0d7c46ac48257a16e53692
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2024-07-21 16:33:54 +0900

    add:redshift_role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,6 @@
 # QuickSightのVPC設定用のロール
 ## https://docs.aws.amazon.com/ja_jp/quicksight/latest/user/vpc-creating-a-connection-in-quicksight-console.html
+## https://dev.classmethod.jp/articles/quicksight-new-vpc-multi-az/
 resource "aws_iam_role" "private_connect" {
   name = "private-connect"
   assume_role_policy = data.aws_iam_policy_document.private_connect_assume.json
@@ -38,7 +39,30 @@ data "aws_iam_policy_document" "private_connect" {
 }
 
 resource "aws_iam_policy_attachment" "private_connect" {
-  name = "private_connect"
+  name = "private-connect"
   roles = [ aws_iam_role.private_connect.name ]
   policy_arn = aws_iam_policy.private_connect.arn
+}
+
+# RedshiftからS3にアクセスするロール
+resource "aws_iam_role" "handson_redshift_role" {
+  name = "handson-RedshiftRole"
+  assume_role_policy = data.aws_iam_policy_document.handson_redshift_assume.json
+}
+
+data "aws_iam_policy_document" "handson_redshift_assume" {
+  statement {
+    actions = [ "sts:AssumeRole" ]
+    
+    principals {
+      type = "Service"
+      identifiers = [ "redshift.amazonaws.com" ]
+    }
+  }
+}
+
+resource "aws_iam_policy_attachment" "handson_redshift_role" {
+  name = "handson-RedshiftRole"
+  roles = [ aws_iam_role.handson_redshift_role.name ]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
